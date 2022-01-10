88c705560ed287cab9da628b4b3ed11131c9214c
yuriyvotinov <yuriy.votinov@sbermarket.ru>
2022-01-10 21:41:46 +0300

    first try commit

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	var helloPhrase = "Hello, OTUS!"
+	helloPhrase = stringutil.Reverse(helloPhrase)
+	fmt.Println(helloPhrase)
 }
